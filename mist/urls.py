09f958bbbdb73bb6fb531f37db6f3cfbcd8207b0
Gabriel Young <gabeplaysdrums@live.com>
2015-01-03 02:45:24 -0800

    set up view for studio app unit tests

--- mist/urls.py
+++ mist/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'studio.views.home', name='home'),
+    url(r'^tests/$', 'studio.views.tests', name='tests'),
 )
--- studio/views.py
+++ studio/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.templatetags.static import static as static_url
 
 
 def home(request):
     return render(request, 'base.html')
+
+
+def tests(request):
+    return render(request, 'qunit.html', dict(
+        title='Mist Studio Unit Tests',
+        tests_url=static_url('tests/studio.js'),
+    ))
